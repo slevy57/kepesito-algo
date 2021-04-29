cbc64fdee62a90d308a8f428e31e3c2385d51a41
Straub Levente <slevy57@gmail.com>
2021-04-29 12:58:47 +0200

    refactoring

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -7,19 +7,17 @@ public class AddDigits {
         if (input.equals("") || input.equals(null)) {
             return -1;
         }
-
         int sumDigits = 0;
 
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
             if (Character.isDigit(c)) {
-                int b = Integer.parseInt(String.valueOf(c));
-                sumDigits = sumDigits + b;
+                int x = Integer.parseInt(String.valueOf(c));
+                sumDigits = sumDigits + x;
             }
-
         }
-            return sumDigits;
-        }
+        return sumDigits;
     }
+}
 
 
